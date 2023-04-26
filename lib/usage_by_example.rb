10bd2434ef40c44e755b7a42c5fb7f42445bfc4a
Adrian Kuhn <akuhn@iam.unibe.ch>
2023-04-26 13:57:46 +0100

    Copied code from other project.

--- lib/usage_by_example.rb
+++ lib/usage_by_example.rb
@@ -2,7 +2,122 @@
 
 require_relative "usage_by_example/version"
 
-module UsageByExample
-  class Error < StandardError; end
-  # Your code goes here...
+
+class UsageByExample
+
+  def self.read(data)
+    return new data.read
+  end
+
+  def initialize(text)
+    @usage = text.gsub('$0', File.basename($0)).gsub(/\n+\Z/, "\n\n")
+
+    # ---- 1) Parse argument names ------------------------------------
+    #
+    # Parse the usage string and extract both optional argument names
+    # and required argument names, for example:
+    #
+    # Usage: connect [options] [mode] host port
+
+    text =~ /Usage: (\w+|\$0) \[options\](( \[\w+\])*)(( \w+)*)/
+    raise RuntimeError, "Expected usage string, got none" unless $1
+    @argument_names_optional = $2.to_s.split.map { |match| match.tr('[]', '').downcase }
+    @argument_names_required = $4.to_s.split.map(&:downcase)
+
+    # ---- 2) Parse option names --------------------------------------
+    #
+    # Parse the usage message and extract option names, their short and
+    # long forms, and the associated argument name (if any), eg:
+    #
+    # Options:
+    #   -s, --secure        Use secure connection
+    #   -v, --verbose       Enable verbose output
+    #   -r, --retries NUM   Number of connection retries (default 3)
+    #   -t, --timeout NUM   Connection timeout in seconds (default 10)
+
+    @option_names = {}
+    text.scan(/((--?\w+)(, --?\w+)*) ?(\w+)?/) do
+      opts = $1.split(", ")
+      opts.each { |each| @option_names[each] = [opts.last.tr('-', ''), $4&.downcase] }
+    end
+
+    # ---- 3) Include help option by default --------------------------
+
+    @option_names.update("-h" => :help, "--help" => :help)
+  end
+
+  def parse(argv)
+    array = argv.dup
+    @arguments = {}
+    @options = {}
+
+    # --- 1) Parse options --------------------------------------------
+
+    most_recent_option = nil
+    until array.empty? do
+      break unless array.first.start_with?(?-)
+      most_recent_option = option = array.shift
+      option_name, argument_name = @option_names[option]
+      raise "Got unknown option #{option}" if option_name.nil?
+      raise if option_name == :help # Show usage without error message
+      @options[option_name] = true
+
+      # Consume argument, if expected by most recent option
+      if argument_name
+        argument = array.shift
+        raise "Expected argument for option #{option}" unless /^[^-]/ === argument
+        @arguments[option_name] = argument
+        most_recent_option = nil
+      end
+    end
+
+    # --- 2) Parse optional arguments ---------------------------------
+
+    stash = array.pop(@argument_names_required.length)
+    @argument_names_optional.each do |argument_name|
+      break if array.empty?
+      argument = array.shift
+      raise "Expected more arguments, got option #{option}" unless /^[^-]/ === argument
+      @arguments[argument_name] = argument
+    end
+
+    # --- 3) Parse required arguments ---------------------------------
+
+    @argument_names_required.each do |argument_name|
+      raise "Expected required argument #{argument_name.upcase}, got none" if stash.empty?
+      argument = stash.shift
+      raise "Expected more arguments, got option #{option}" unless /^[^-]/ === argument
+      @arguments[argument_name] = argument
+    end
+
+    # --- 4) Expect to be done ----------------------------------------
+
+    if not array.empty?
+      # Custom error message if most recent option did not require argument
+      raise "Got unexpected argument for option #{most_recent_option}" if most_recent_option
+      min_length = @argument_names.length
+      max_length = @argument_names.length + @argument_names_optional.length
+      raise "Expected #{min_length}#{"-#{max_length}" if max_length > min_length} arguments, got more"
+    end
+
+    return self
+
+  rescue RuntimeError => err
+    puts "ERROR: #{err.message}\n\n" unless err.message.empty?
+    puts @usage
+    exit
+  end
+
+  def method_missing(sym, *args, &block)
+    case sym
+    when /^argument_(.*)$/
+      val = @arguments[$1]
+      block && val ? block.call(val) : val
+    when /^include_(.*)\?$/
+      @options[$1]
+    else
+      super
+    end
+  end
 end
+
--- lib/usage_by_example/version.rb
+++ lib/usage_by_example/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-module UsageByExample
-  VERSION = "0.1.0"
+class UsageByExample
+  VERSION = "0.0.0"
 end
