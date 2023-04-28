06fe06778d0b7c9993459fc7d09c5aa92f32a83a
Adrian Kuhn <akuhn@iam.unibe.ch>
2023-04-28 22:36:33 +0100

    Move script to bin fodler.

--- bin/run_rspec_with_all_rbenv_versions.rb
+++ bin/run_rspec_with_all_rbenv_versions.rb
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Remove the "BUNDLED WITH" section in Gemfile.lock to prevent potential
+# issues when using different versions of Ruby and Bundler
+sed -i '' '/BUNDLED WITH/,$d' Gemfile.lock
+
+# Enable rbenv shell integration
 eval "$(rbenv init -)"
 
 for version in $(rbenv versions --bare); do
