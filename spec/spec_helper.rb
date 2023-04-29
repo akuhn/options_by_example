3ef960e152210bcccc2a9e6f5e2908be3347b783
Adrian Kuhn <akuhn@iam.unibe.ch>
2023-04-30 00:19:49 +0100

    Catch unexpected exits.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,12 @@ require 'options_by_example'
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = ".rspec_status"
+
+  config.around :example do |example|
+    begin
+      example.run
+    rescue SystemExit => err
+      fail "Example called exit with status #{err.status}, program ended prematurely"
+    end
+  end
 end
