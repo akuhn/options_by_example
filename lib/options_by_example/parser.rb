2608751665a84608efc4b659df6d7449d9b55063
Joe Example <joe@example.com>
2026-01-21 16:54:26 +0000

    Better variable names

--- lib/options_by_example/parser.rb
+++ lib/options_by_example/parser.rb
@@ -11,8 +11,8 @@ class OptionsByExample
 
   class Parser
 
-    attr_reader :options
-    attr_reader :arguments
+    attr_reader :option_values
+    attr_reader :argument_values
 
     def initialize(argument_names_required, argument_names_optional, default_values, option_names)
       @argument_names_required = argument_names_required
@@ -20,8 +20,8 @@ class OptionsByExample
       @default_values = default_values
       @option_names = option_names
 
-      @arguments = @default_values.dup
-      @options = {}
+      @argument_values = @default_values.dup
+      @option_values = {}
     end
 
     def parse(array)
@@ -30,16 +30,16 @@ class OptionsByExample
       # chunks, plus tracking leading excess arguments. This organization
       # facilitates further processing and validation of the input.
 
-      @chunks = []
+      @slices = []
       @remainder = current = []
       array.each do |each|
-        @chunks << current = [] if each.start_with?(?-)
+        @slices << current = [] if each.start_with?(?-)
         current << each
       end
 
-      detect_help_option
-      flatten_stacked_shorthand_options
-      detect_unknown_options
+      raise_if_help_option
+      unpack_combined_shorthand_options
+      raise_if_unknown_options
       parse_options
 
       validate_number_of_arguments
@@ -51,8 +51,8 @@ class OptionsByExample
 
     private
 
-    def detect_help_option
-      @chunks.each do |option, *args|
+    def raise_if_help_option
+      @slices.each do |option, *args|
         case option
         when '-h', '--help'
           raise PrintUsageMessage
@@ -60,7 +60,7 @@ class OptionsByExample
       end
     end
 
-    def flatten_stacked_shorthand_options
+    def unpack_combined_shorthand_options
 
       # Expand any combined shorthand options like -svt into their
       # separate components (-s, -v, and -t) and assigns any arguments
@@ -68,7 +68,7 @@ class OptionsByExample
       # a helpful error message with suggestion if possible.
 
       list = []
-      @chunks.each do |option, *args|
+      @slices.each do |option, *args|
         if option =~ /^-([a-zA-Z]{2,})$/
           shorthands = $1.each_char.map { |char| "-#{char}" }
 
@@ -86,23 +86,23 @@ class OptionsByExample
         end
       end
 
-      @chunks = list
+      @slices = list
     end
 
-    def detect_unknown_options
-      @chunks.each do |option, *args|
+    def raise_if_unknown_options
+      @slices.each do |option, *args|
         raise "Found unknown option '#{option}'" unless @option_names.include?(option)
       end
     end
 
     def parse_options
-      @chunks.each do |option, *args|
+      @slices.each do |option, *args|
         if @remainder.any?
           raise "Unexpected arguments found before option '#{option}', please provide all options before arguments"
         end
 
         option_name, argument_name = @option_names[option]
-        @options[option_name] = true
+        @option_values[option_name] = true
 
         if argument_name
           raise "Expected argument for option '#{option}', got none" if args.empty?
@@ -124,7 +124,7 @@ class OptionsByExample
             raise "Invalid argument \"#{value}\" for option '#{option}', please provide #{expected_type}"
           end
 
-          @arguments[option_name] = value
+          @argument_values[option_name] = value
           @option_took_argument = option
         else
           @option_took_argument = nil
@@ -154,14 +154,14 @@ class OptionsByExample
       stash = @remainder.pop(@argument_names_required.length)
       @argument_names_required.each do |argument_name|
         raise "Missing required argument '#{argument_name}'" if stash.empty?
-        @arguments[argument_name] = stash.shift
+        @argument_values[argument_name] = stash.shift
       end
     end
 
     def parse_optional_arguments
       @argument_names_optional.each do |argument_name|
         break if @remainder.empty?
-        @arguments[argument_name] = @remainder.shift
+        @argument_values[argument_name] = @remainder.shift
       end
     end
   end
